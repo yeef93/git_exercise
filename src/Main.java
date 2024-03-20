c0374fd98271a26ffc4663c0cb973ec7adcc026a
yeef93 <yunindafaranika@gmail.com>
2024-03-20 15:14:28 +0700

    feat:deveplopment Add more message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Change text from print");
+        System.out.println("Change text from print");
+        System.out.println("Change text from print");
+        System.out.println("Change text from print");
+        System.out.println("Change text from print");
     }
 }
